656f910c7db13590266140d137d204ae6204a8e3
esteban@eviniegra.software <esteban@eviniegra.software>
2025-07-11 13:45:36 +0200

    V1 fucnionando

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,13 @@
 {
     'name': "POS Analytic in Sale Report",
     'version': '15.0.1.0.0',
-    'summary': 'Transfiere la cuenta analítica del POS a los reportes de ventas.',
-    'author': 'Tu nombre',
+    'summary': 'Asigna cuenta analítica a líneas POS y asegura el campo en el reporte de ventas',
+    'author': 'Esteban Viniegra Pérez Olagaray | Pridecta',
     'license': 'AGPL-3',
-    'website': '',
-    'depends': [
-        'pos_sale',
-        'pos_analytic_by_config',
-    ],
-    'data': [
-        'views/sale_report_patch.xml',
-    ],
+    'website': 'https://pridecta.com',
+    'depends': ['pos_order__line','sale','pos_analytic_by_config'],
+    'data': [],
+    'icon': 'pos_sale_analytic_patch/static/description/logo.png',
     'installable': True,
     'auto_install': False,
 }
--- models/pos_order_line.py
+++ models/pos_order_line.py
@@ -1,18 +1,21 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
     analytic_account_id = fields.Many2one(
         'account.analytic.account',
-        string='Cuenta analítica',
-        readonly=True
+        string='Analytic Account',
+        store=True,
+        compute='_compute_analytic_account_id',
     )
 
-    @api.model
-    def create(self, vals):
-        if 'order_id' in vals and not vals.get('analytic_account_id'):
-            order = self.env['pos.order'].browse(vals['order_id'])
-            if order.session_id.config_id.account_analytic_id:
-                vals['analytic_account_id'] = order.session_id.config_id.account_analytic_id.id
-        return super().create(vals)
+    @api.depends('order_id.session_id.config_id.account_analytic_id')
+    def _compute_analytic_account_id(self):
+        for line in self:
+            analytic = (
+                line.order_id.session_id.config_id.account_analytic_id
+                if line.order_id and line.order_id.session_id and line.order_id.session_id.config_id
+                else False
+            )
+            line.analytic_account_id = analytic.id if analytic else False
