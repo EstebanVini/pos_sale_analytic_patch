00f79864c6ff61d927b733cd4c3489243dbf553a
esteban@eviniegra.software <esteban@eviniegra.software>
2025-07-14 12:03:04 +0200

    Upload to the app store

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,16 @@
 {
     'name': "POS Analytic in Sale Report",
     'version': '15.0.1.0.0',
-    'summary': 'Asigna cuenta analítica a líneas POS y asegura el campo en el reporte de ventas',
+    'summary': 'Assign analytic account to POS lines and ensure the field in the sales report',
     'author': 'Esteban Viniegra Pérez Olagaray | Pridecta',
     'license': 'AGPL-3',
-    'website': 'https://pridecta.com',
+    'website': 'https://pridecta.es',
     'depends': ['sale','pos_analytic_by_config'],
     'data': [],
-    'icon': 'pos_sale_analytic_patch/static/description/logo.png',
+    'icon': 'pos_sale_analytic_patch/static/description/icon.png',
+    'images': [
+        'static/description/cover.png',
+    ],
     'installable': True,
     'auto_install': False,
 }
